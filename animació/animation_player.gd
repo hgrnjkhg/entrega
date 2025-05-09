5ccb3663b2d162508c8ae8426cac72deb2fbb79a
hgrnjkhg <205313624+hgrnjkhg@users.noreply.github.com>
2025-05-09 16:40:20 +0200

    script del stitch

---
+++ animació/animation_player.gd
@@ -0,0 +1,10 @@
+extends AnimationPlayer
+func _ready() -> void:
+	speed_scale = 0.6
+	play("tutorial")
+	
+	
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	print("finished")
+	play("RESET")
